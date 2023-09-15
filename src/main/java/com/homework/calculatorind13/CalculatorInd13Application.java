eccc04c262fccd657bc892705df362a44810559a
CymbalMonkey <xeniolum@mail.ru>
2023-09-15 14:26:18 +0300

    initial commit

---
+++ src/main/java/com/homework/calculatorind13/CalculatorInd13Application.java
@@ -0,0 +1,13 @@
+package com.homework.calculatorind13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorInd13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorInd13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/calculatorind13/CalculatorInd13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.calculatorind13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorInd13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
