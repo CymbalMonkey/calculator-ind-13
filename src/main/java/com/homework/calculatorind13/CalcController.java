4a826bcd6aa0f7df7aec4fdf263cb746ea0c51e0
CymbalMonkey <xeniolum@mail.ru>
2023-09-18 14:38:26 +0300

    refactor

--- src/main/java/com/homework/calculatorind13/CalcController.java
+++ src/main/java/com/homework/calculatorind13/CalcController.java
@@ -22,21 +22,28 @@ public class CalcController {
 
     @GetMapping("/plus")
     public String plus(@RequestParam int num1, @RequestParam int num2) {
-        return calculatorService.plus(num1, num2);
+        return formatResult(num1, num2, "+", String.valueOf(calculatorService.plus(num1, num2)));
     }
 
     @GetMapping("/minus")
     public String minus(@RequestParam int num1, @RequestParam int num2) {
-        return calculatorService.minus(num1, num2);
+        return formatResult(num1, num2, "-", String.valueOf(calculatorService.minus(num1, num2)));
     }
 
     @GetMapping("/multiply")
     public String multiply(@RequestParam int num1, @RequestParam int num2) {
-        return calculatorService.multiply(num1, num2);
+        return formatResult(num1, num2, "*", String.valueOf(calculatorService.multiply(num1, num2)));
     }
 
     @GetMapping("/divide")
     public String divide(@RequestParam int num1, @RequestParam int num2) {
-        return calculatorService.divide(num1, num2);
+        if (num2 == 0) {
+            return "На ноль делить нельзя";
+        }
+        return formatResult(num1, num2, "/", String.valueOf(calculatorService.divide(num1, num2)));
+    }
+
+    private String formatResult(int num1, int num2, String operation, String result) {
+        return String.format("%s %s %s = %s", num1, operation, num2, result);
     }
 }
--- src/main/java/com/homework/calculatorind13/CalculatorService.java
+++ src/main/java/com/homework/calculatorind13/CalculatorService.java
@@ -9,26 +9,19 @@ public class CalculatorService {
         return "Добро пожаловать в калькулятор";
     }
 
-    public String plus(int num1, int num2) {
-        return formatResult(num1, num2, "+", Integer.toString(num1 + num2));
+    public int plus(int num1, int num2) {
+        return (num1 + num2);
     }
 
-    public String minus(int num1, int num2) {
-        return formatResult(num1, num2, "-", Integer.toString(num1 - num2));
+    public int minus(int num1, int num2) {
+        return (num1 - num2);
     }
 
-    public String multiply(int num1, int num2) {
-        return formatResult(num1, num2, "*", String.valueOf(num1 * num2));
+    public int multiply(int num1, int num2) {
+        return (num1 * num2);
     }
 
-    public String divide(int num1, int num2) {
-        if (num2 == 0) {
-            return "На ноль делить нельзя";
-        }
-        return formatResult(num1, num2, "/", String.valueOf((double) num1 / num2));
-    }
-
-    private String formatResult(int num1, int num2, String operation, String result) {
-        return String.format("%s %s %s = %s", num1, operation, num2, result);
+    public double divide(int num1, int num2) {
+        return ((double) num1 / num2);
     }
 }
